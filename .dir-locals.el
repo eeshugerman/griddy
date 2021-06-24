95c49995716c97eba127decb8bbc30c560c7e6d9
Elliott Shugerman <eeshugerman@gmail.com>
2021-06-23 22:44:40 -0400

    bad .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+;; TODO: do better
+((scheme-mode . ((geiser-guile-binary . "/gnu/store/jzzdg9wnbjq8qi8628wlzsv25dzcmk3h-profile/bin/guile")
+                 (geiser-guile-load-path . ("/gnu/store/jzzdg9wnbjq8qi8628wlzsv25dzcmk3h-profile/lib/guile/3.0/site-ccache")))))
