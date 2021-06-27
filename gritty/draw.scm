658f78cc3072aed2254153b5cde385ac2cf816fb
Elliott Shugerman <eeshugerman@gmail.com>
2021-06-27 00:45:14 -0400

    more basic road drawing stuff
    - mostly fix alignment issues
    - clean up example

--- gritty/draw.scm
+++ gritty/draw.scm
@@ -6,37 +6,16 @@
   #:use-module (gritty math)
   #:export (draw))
 
-(define *draw/road-junction/size* 10)
+(define *draw/road-junction/size* 50)
 (define *draw/road-junction/color* "grey")
-(define *draw/road-segment/width* 5)
+(define *draw/road-segment/width* 40)
 
 (define (square size)
   (rectangle size size))
 
-(define-method (draw (w <world>))
-  (let* ((world-pict
-          (remove-outline (rectangle (size-x w) (size-y w))))
-         (populated-world-pict
-          (fold draw-many-over
-                world-pict
-                (list (road-junctions w)
-                      (road-segments w)))))
-    populated-world-pict))
-
-(define (draw-many-over items base-pict)
-  (fold draw-over base-pict items))
-
 (define-method (draw (j <road-junction>))
   (fill (square *draw/road-junction/size*)
         *draw/road-junction/color*))
-
-(define-method (draw-over (item <road-junction>) base-pict)
-  (let* ((item-pict (draw item))
-         (x (- (pos-x item)
-               (/2 (pict-width item-pict))))
-         (y (- (pos-y item)
-               (/2 (pict-height item-pict)))))
-    (pin-over base-pict x y (draw item))))
 
 (define-method (draw (s <road-segment>))
   (line (pos-x (start-junction s))
@@ -45,6 +24,29 @@
         (pos-y (stop-junction s))
         #:stroke-width *draw/road-segment/width*))
 
-(define-method (draw-over (item <road-segment>) base-pict)
-  (pin-over base-pict 0 0 (draw item)))
+(define-method (draw-over (j <road-junction>) base-pict)
+  (let* ((junction-pict (draw j))
+         (x (- (pos-x j)
+               (/2 (pict-width junction-pict))))
+         (y (- (pos-y j)
+               (/2 (pict-height junction-pict)))))
+    (pin-over base-pict x y junction-pict)))
+
+(define-method (draw-over (s <road-segment>) base-pict)
+  (let ((offset (exact->inexact (/ *draw/road-segment/width* 4)))
+        (segment-pict (draw s)))
+    (pin-over base-pict offset offset segment-pict)))
+
+(define (draw-many-over items base-pict)
+  (fold draw-over base-pict items))
+
+(define-method (draw (w <world>))
+  (let* ((world-pict
+          (remove-outline (rectangle (size-x w) (size-y w))))
+         (populated-world-pict
+          (fold draw-many-over
+                world-pict
+                (list (road-segments w)
+                      (road-junctions w)))))
+    populated-world-pict))
 
