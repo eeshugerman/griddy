ae5b8302de3da83fea026a07a3f11dd1c2cbb144
Elliott Shugerman <eeshugerman@gmail.com>
2021-07-14 22:50:54 -0400

    do/turn-onto (wipppp) (definitely broke stuff)

--- griddy/simulate.scm
+++ griddy/simulate.scm
@@ -46,7 +46,10 @@
          (pos-param-current (get actor 'location 'pos-param))
          (pos-param-delta (* *simulate/time-step*
                              (/ (get actor 'max-speed)
-                                (length-of (get lane-current 'segment)))))
+                                (length-of (get lane-current 'segment)))
+                             (match direction-current
+                               ('forward +1)
+                               ('backward -1))))
          (lane++ (get-static++ lane-current))
          (actor++ (copy actor)))
 
@@ -58,57 +61,42 @@
 
     (define (do/arrive-at pos-param-target)
       (let* ((pos-param-to-target (- pos-param-target pos-param-current)))
-        (if (>= pos-param-delta (abs pos-param-to-target))
+        (if (>= (abs pos-param-delta) (abs pos-param-to-target))
             (let ((location++ (make <location>
                                 #:road-lane lane++
                                 #:pos-param pos-param-target)))
               (pop-route! actor++)
               (link! actor++ location++))
             ;; continue on segment
-            (let* ((pos-param++ ((match current-direction
-                                   ('forward +)
-                                   ('backward -))
-                                 pos-param-current
-                                 pos-param-delta))
+            (let* ((pos-param++ (+ pos-param-current pos-param-delta))
                    (location++ (make <location>
                                  #:road-lane lane++
                                  #:pos-param pos-param++)))
               (link! actor++ location++)))))
 
     (define (do/turn-onto lane-next)
-      (let* ((pos-param-to-target
-              (match current-direction
-                ('forward (- 1 pos-param-current))
-                ('backward pos-param-current))))
-        (if (>= pos-param-delta (abs pos-param-to-target))
-            ;; time to turn
-            (let ((direction-next (get lane-next 'direction))
-                  (pos-param-remainder (- pos-param-delta pos-param-to-junction))
-                  (pos-param++ (match direction-next
-                                 ('forward pos-param-remaining)
-                                 ('backward (- 1 pos-param-remainder))))
-                  (lane-next++ (get-static++ lane-next))
-                  (location++ (make <location>
-                                #:road-lane lane-next++
-                                #:pos-param pos-param++)))
-              (pop-route! actor++)
-              (link! actor++ location++))
-            ;; continue on segment
-            (let* ((pos-param++ ((match current-direction
-                                   ('forward +)
-                                   ('backward -))
-                                 pos-param-current
-                                 pos-param-delta))
-                   (location++ (make <location>
-                                 #:road-lane lane++
-                                 #:pos-param pos-param++)))
-              (link! actor++ location++)))))
+      (let* ((pos-param++ (+ pos-param-current pos-param-delta))
+             (lane-next++ (get-static++ lane-next)))
+
+        (match (cons current-direction pos-param++)
+          (('forward .  (? > pos-param++ 1)) ;; time to turn
+           ...
+           (pop-route! actor++)
+           (link! actor++ location++))
+          (('backward . (? < pos-param++ 0)) ;; time to turn
+           ...
+           (pop-route! actor++)
+           (link! actor++ location++))
+          ((' forward . _)
+           )
+          (('backward . _)
+           ))))
 
     (match (get actor 'route)
       (()
        (do/=nil=))
       ((('arrive-at pos-param) rest ...)
        (do/arrive-at pos-param))
-      ;; ((('turn-onto road-lane) rest ...)
-      ;;  (do/turn-onto road-lane))
+      ((('turn-onto road-lane) rest ...)
+       (do/turn-onto road-lane))
       )))
