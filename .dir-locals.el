e92a830c80efef69087740ebe7c0e47fdacb14d5
Elliott Shugerman <eeshugerman@gmail.com>
2021-10-14 12:17:53 -0400

    dir-locals cleanup and manifest comment

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,9 +1,6 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-;; TODO: do better
-((scheme-mode . ((geiser-guile-binary . "/gnu/store/iy525ncl8bmvm84bx0cwqmshjh1cjmd5-profile/bin/guile")
-                 (geiser-guile-load-path . ("/gnu/store/iy525ncl8bmvm84bx0cwqmshjh1cjmd5-profile/lib/guile/3.0/site-ccache"
-                                            "/home/elliott/devel/griddy"))
-                 (eval . (put 'with-style 'scheme-indent-function 1))
-                 (eval . (put 'match-direction 'scheme-indent-function 1)))))
+((scheme-mode . ((eval . (put 'with-style 'scheme-indent-function 1))
+                 (eval . (put 'match-direction 'scheme-indent-function 1))
+                 (eval . (put 'pipe 'scheme-indent-function 1)))))
--- manifest.scm
+++ manifest.scm
@@ -1,6 +1,7 @@
 ;;; GUIX_PROFILE=~/.guix-extra-profiles/griddy
 ;;; guix pull -C channels-spec.scm && guix package -u
 ;;; guix package -m manifest.scm
+;;; guix environment -p $GUIX_PROFILE  # can skip to this one after initial setup
 
 (specifications->manifest
  '("guile@3.0.7"
